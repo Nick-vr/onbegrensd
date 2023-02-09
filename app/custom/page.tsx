f27192a6894ebf697495dd825e137194ccb30f26
Nick <nvanroyen@gmail.com>
2023-02-09 21:35:06 +0100

    fixed scss module names

--- app/custom/page.tsx
+++ app/custom/page.tsx
@@ -1,9 +1,18 @@
 // components
 // assets
 // styles
+import S from './custom.module.scss'
 
 const Custom = () => {
-  return <div>Custom</div>
+  return (
+    <div className={S.customWrapper}>
+      <h1 className={S.title}>Custom</h1>
+      <p className={S.intro}>
+        Kies een kledingstuk. <br />
+        Creëer je eigen stijl.
+      </p>
+    </div>
+  )
 }
 
 export default Custom
